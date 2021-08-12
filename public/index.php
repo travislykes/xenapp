3d852e3da20237abc438eda951d487bdf62bda3c
travislykes <fopokuamoako@gmail.com>
2021-08-12 08:51:24 +0200

    added routing package

--- public/index.php
+++ public/index.php
@@ -1,3 +1,14 @@
 <?php
 
 require __DIR__.'/../vendor/autoload.php';
+
+// Use this namespace
+use Steampixel\Route;
+
+// Add your first route
+Route::add('/', function() {
+    echo 'Welcome :-)';
+});
+
+// Run the router
+Route::run('/');
